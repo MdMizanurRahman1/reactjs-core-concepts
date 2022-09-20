9485f8c9055de89ebbb85c1d4f4d77d2e1df8496
MdMizanurRahman1 <mizan.rahman2217@gmail.com>
2022-09-20 15:23:00 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { buildQueries } from '@testing-library/react';
+
+const number = 5555;
+const singer = {
+  name: 'Md Mizanur Rahman', job: 'unemployed'
+};
+const singer2 = { name: 'Mohsina Jahan', job: 'wife' };
+
+const singerStyle = {
+  color: 'blue',
+  backgroundColor: 'white'
+
+}
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
+        <h1>JSX</h1>
+        <div className="container">
+          <h3>Hello Dude, React. How are you?</h3>
+        </div>
+
+        <div className="music">
+          <p>Name: {2222 + number}</p>
+          <p style={singerStyle}>Name: {singer.name} {singer.job} </p>
+          <p style={{
+            color: 'green',
+            backgroundColor: 'white'
+          }}>Name: {singer2.name} {singer2.job}</p>
+        </div>
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
